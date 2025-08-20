c599b45479a2660aa72e54555f8cc30b8181df97
Lovmong <1967471283@qq.com>
2025-08-20 17:56:48 +0800

    add conflict services parameter

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from i2c_iap_tool.app import IapToolApp
-from globals.fusion_hat_globals import Fusion_HAT_Globals
+from globals.fusion_hat_globals import Fusion_HAT_Globals, CONFLICT_SERVICES
 
 if __name__ == '__main__':
-    app = IapToolApp(globals=Fusion_HAT_Globals)
+    app = IapToolApp(globals=Fusion_HAT_Globals, conflict_services=CONFLICT_SERVICES)
     app.run()
